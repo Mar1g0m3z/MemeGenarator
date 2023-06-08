0dac60e2476bdd52f4d161b1b52cb7a6c8ed99d7
Anderis <88600844+Anderis@users.noreply.github.com>
2023-06-07 21:16:56 -0500

    Final Merge, Online

--- bot.js
+++ bot.js
@@ -1,18 +1,21 @@
-const Discord = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
-const client = new Discord.Client();
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
 
 client.on('ready', () => {
   console.log(`Bot is online and logged in as ${client.user.tag}`);
 });
 
-client.on('message', (message) => {
+client.on('messageCreate', (message) => {
   // Handle incoming messages here
 });
-
-client.on('error', (error) => {
-    console.error('An error occurred:', error);
-  });
 
 client.login(process.env.TOKEN);
